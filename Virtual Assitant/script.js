81fa8a03e7581d9e60742c232f9666317baaa63e
yashasvisakshi2005 <yashasvisakshi2005@gmail.com>
2025-07-09 15:47:13 +0000

    virtual assistant

--- Virtual Assitant/script.js
+++ Virtual Assitant/script.js
@@ -6,6 +6,23 @@ function speak(text) {
     text_speak.rate = 1;
     text_speak.pitch = 1;
     text_speak.volume = 1;
-    text_speak.lang = 'en-US'; 
+    text_speak.lang = "hi-GB"
     window.speechSynthesis.speak(text_speak);
 }
+
+function wishMe(){
+    let day=new Date()
+   let hours = day.getHours()
+   if(hours>=0 && hours<12){
+    speak("Good Morning Sir")
+   }
+   else if(hours>=12 && hours <16){
+    speak("Good Afternoon Sir")
+   }
+   else{
+    speak("Good Evening Sir")
+   }
+}
+window.addEventListener('load',()=>{
+    wishMe()
+})
